f23489b8b8e6490421fa770e3ddf5d9a200c06ed
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2015-03-18 20:20:47 +0000

    adding more comments

--- cachematrix.R
+++ cachematrix.R
@@ -13,13 +13,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
+  # The set function sets the value of the matrix (x) by using <<-
+  # to set it in the defining environment. It sets the inverse to NULL since the
+  # matrix has changed, so the old inverse(if any) is no longer valid.
   set <- function(y) {
     x <<- y
     inverse <<- NULL
   }
+  #returns the matrix
   get <- function() x
+  # sets the inverse
   setinverse <- function(inv) inverse <<- inv
+  # returns the inverse
   getinverse <- function() inverse
+  
+  # when you call makeCacheMatrix it will return those 4 functions
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -37,10 +45,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinverse()
+  # if inverse found in cache, then will return it
   if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
   }
+  # otherwise, will get the matrix, calculate the inverse, save it in the cache
+  # Then return that result.
   data <- x$get()
   inverse <- solve(data, ...)
   x$setinverse(inverse)
